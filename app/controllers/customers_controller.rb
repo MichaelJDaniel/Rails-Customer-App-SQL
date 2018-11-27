1d0f4934ac024fed0ce97224d0b1005c297de14d
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-27 11:29:29 -0700

    built customer controller AND raw sql querries

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,40 @@
+class CustomersController < ApplicationController
+  # SELECT all records
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+  def index
+    @customers = Customer.all_customers(current_user.id)
+  end
+
+  def show
+    
+  end
+
+  def new
+    @customer = current_user.customers.new
+  end
+
+  def create
+    # INSERT one record
+    Customer.create_customer(customer_params, current_user.id, params[:id])
+  end
+
+  def edit
+  end
+
+  def update 
+  end
+
+  def destroy
+  end
+
+
+  private 
+
+  def set_customer
+    @customer = Customer.single_customer(current_user.id, params[:id])
+  end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :email, :phone)
+  end
+end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,49 @@
 class Customer < ApplicationRecord
   belongs_to :user
+
+  # Instance Method 
+  # customer = { first_name "", last_name ""}
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.all_customers(user_id)
+    Customer.find_by_sql(["
+      SELECT *
+      FROM customers AS c
+      WHERE c.user_id = ?
+      ", user_id])
+  end
+
+
+  def self.single_customer(user_id, customer_id)
+    Customer.find_by_sql (["
+      SELECT *
+      FROM customers AS c
+      WHERE c.user_id = ? 
+      AND c.id = ?
+      ", user_id, customer_id]).first
+  end
+
+  def self.create_customer(params, user_id)
+    Customer.find_by_sql(["
+      INSERT INTO customers (first_name, last_name, email, phone, user_id, created_at, updated_at)
+      VALUES (:first, :last, :email, :phone, :user_id, :created_at, :updated_at)
+      ", { 
+        first: p[:first_name],
+        last: p[:last_name],
+        email: p[:email],
+        phone: p[:phone],
+        user_id: user_id,
+        created_at :DateTime.now,
+        updated_at :DateTime.now
+      }])
+  end
+
+  def self.delete_customer(customer_id)
+    Customer.find_by_sql(["
+      DELETE FROM customers AS c
+      WHERE c.id = ?
+    ;", customer_id])
+  end
 end
